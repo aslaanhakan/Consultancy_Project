54da3afe37cd70747ca3e6e0efc05aa925581125
aslaanhakan <111008071+aslaanhakan@users.noreply.github.com>
2023-04-12 17:52:50 +0300

    Admin area is being organized

---
+++ Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/ViewComponents/AdminNavbarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Consultancy_Project.Entity.Concrate.Identity;
+using Consultancy_Project.MVC.Areas.Admin.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Consultancy_Project.MVC.Areas.Admin.ViewComponents
+{
+    [Area("Admin")]
+    public class AdminNavbarViewComponent: ViewComponent
+    {
+        private UserManager<User> _userManager;
+
+        public AdminNavbarViewComponent(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var admin = await _userManager.GetUsersInRoleAsync("Admin");
+            UserViewModel users = _userManager.Users.Where(x=>x.FirstName==admin.FirstOrDefault().FirstName).Select(u => new UserViewModel
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                ImageUrl = u.Images.Url
+            }).FirstOrDefault();
+           
+            return View(users);
+        }
+    }
+}
