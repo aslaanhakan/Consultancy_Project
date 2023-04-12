95af860f3d9a1336ca7d26234a418b5cd600500f
aslaanhakan <111008071+aslaanhakan@users.noreply.github.com>
2023-04-12 10:58:07 +0300

    Admin area is being organized

--- Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Controllers/HomeController.cs
+++ Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Consultancy_Project.Entity.Concrate.Identity;
+using Consultancy_Project.MVC.Areas.Admin.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Consultancy_Project.MVC.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class HomeController : Controller
     {
+        private readonly UserManager<User> _userManager;
+
+        public HomeController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<UserViewModel> users = _userManager.Users.Select(u=> new UserViewModel
+            {
+                FirstName = u.FirstName,
+                LastName = u.LastName,
+                ImageUrl=u.Images.Url,
+                               
+            }).ToList();
+            return View(users);
         }
     }
 }
---
+++ Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Models/ViewModels/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Consultancy_Project.MVC.Areas.Admin.Models.ViewModels
+{
+    public class UserViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
