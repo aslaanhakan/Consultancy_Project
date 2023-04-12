49fa4603bb4f93787de738fc9989290f86829b21
aslaanhakan <111008071+aslaanhakan@users.noreply.github.com>
2023-04-12 15:31:35 +0300

    Template added, editing in progress.

--- Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Controllers/HomeController.cs
+++ Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Controllers/HomeController.cs
@@ -21,8 +21,7 @@ namespace Consultancy_Project.MVC.Areas.Admin.Controllers
             {
                 FirstName = u.FirstName,
                 LastName = u.LastName,
-                ImageUrl=u.Images.Url,
-                               
+                ImageUrl=u.Images.Url
             }).ToList();
             return View(users);
         }
