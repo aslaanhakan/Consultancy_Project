458570b6224445bfb94dabaad9c9dfad50f8eac9
aslaanhakan <111008071+aslaanhakan@users.noreply.github.com>
2023-04-14 10:47:21 +0300

    Admin panel is being edited

---
+++ Consultancy_Project/Consultancy_Project.MVC/Areas/Admin/Controllers/AdministratorController.cs
@@ -0,0 +1,35 @@
+﻿using Consultancy_Project.Entity.Concrate.Identity;
+using Consultancy_Project.MVC.Areas.Admin.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace Consultancy_Project.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+
+    public class AdministratorController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public AdministratorController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admin = await _userManager.GetUsersInRoleAsync("Admin");
+            List<UserViewModel> adminWiew = admin.Select(a=>new UserViewModel
+            {
+                FirstName= a.FirstName,
+                LastName= a.LastName,
+            }).ToList();
+            return View(adminWiew);
+        }
+    }
+}
